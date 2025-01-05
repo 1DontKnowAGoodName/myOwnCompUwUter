de69e557b85e50d301fc9651ce288cb0184d8cac
peter <neuron3000@outlook.com>
2025-01-05 13:09:24 +0100

    first commit

---
+++ assembler.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+
+  //check if input file can be opened  //give it a funky name so im not suddenly editing system files
+  //open file
+
+  //check if output can be opened      //give it a funky name so im not suddenly editing system files 
+  //open file
+
+  //int i = lines(inp.txt)
+  
+  //for i:
+    
+    //read line(inp.txt(i));
+    
+    //translate mnemonic           //throw error if this doesn't work throw error and close() and break;
+
+    //add parameters
+
+      //register translation
+      
+      //label fuckery
+
+      //translate immediates
+  
+  //close inp.txt && out.txt;
+  
+
+}
