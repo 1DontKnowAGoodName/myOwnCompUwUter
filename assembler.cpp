290d418c49e05b4eef2c725424e0219537e4645a
peter <neuron3000@outlook.com>
2025-01-07 09:10:19 +0100

    extracts, outputs in console and places in outputfile

--- assembler.cpp
+++ assembler.cpp
@@ -6,34 +6,47 @@ int main(){
   //open files
   //give it a funky name so im not suddenly editing system files
   
-  std::ifstream inpFile{"asmCode"};
-  std::ofstream outpFile{"binAsmCodeUwU"};
+  std::ifstream inpFile{"C:\\Users\\aevdm\\Documents\\CPUproject\\assembler files\\asmCodeUwU.txt"};
+  std::ofstream outpFile{"C:\\Users\\aevdm\\Documents\\CPUproject\\assembler files\\binAsmCodeUwU.txt"};
 
   //check if input file can be opened
   if(!inpFile.is_open()){
-    std::cout << "was not able to open your file";
+    std::cout << "was not able to open input file\n";
     return 1;
   }
   //check if output can be opened
-  if(!inpFile.is_open()){
-    std::cout << "was not able to open your file";
+  if(!outpFile.is_open()){
+    std::cout << "was not able to open output file\n";
     return 1;
   }
-  
+
   std::string inputStr;
+  
+  while(std::getline(inpFile, inputStr)){
 
-  while(getline(std::cin >> std::ws, inputStr)){
+    std::cout << inputStr <<'\n';
+    outpFile << inputStr << '\n';
+
+
+
+
+
+
+
     //translate var.mnemonic           //throw error if this doesn't work throw error and close() and break;
-
+ 
     //add parameters
 
       //register translation
       
-      //label fuckery
+      //label fuckerys
 
       //translate immediates
   }
+  
   //close inp.txt && out.txt;
+  inpFile.close();
+  outpFile.close();
 
   return 0;
 }
